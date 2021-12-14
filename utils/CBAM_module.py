278b94897b2aff6ea593a4016330ce3c24c11243
Alba29 <1030832382@qq.com>
2021-12-14 13:06:35 +0800

    V1.0

---
+++ utils/CBAM_module.py
@@ -0,0 +1,138 @@
+import torch
+from torch import nn
+
+#在ResNet中的每个block中添加了CBAM模块，训练数据来自benchmark ImageNet-1K。检测使用的是Faster R-CNN， Backbone选择的ResNet34,ResNet50, WideResNet18, ResNeXt50等，还跟SE等进行了对比。
+ 
+#SE_Module
+class SE_module(nn.Module):
+    def __init__(self, channel, ratio=16):
+        super(SE_module, self).__init__()
+        self.squeeze = nn.AdaptiveAvgPool2d(1)
+        self.excitation = nn.Sequential(
+                nn.Linear(in_features=channel, out_features=channel // ratio),
+                nn.ReLU(inplace=True),
+                nn.Linear(in_features=channel // ratio, out_features=channel),
+                nn.Sigmoid()
+            )
+    def forward(self, x):
+        b, c, _, _ = x.size()
+        y = self.squeeze(x).view(b, c)
+        z = self.excitation(y).view(b, c, 1, 1)
+        return x*z.expand_as(x)
+
+
+#CBAM 结构代码
+#通道注意力模块
+class ChannelAttention(nn.Module):
+    def __init__(self, in_planes, ratio=16):
+        super(ChannelAttention, self).__init__()
+        self.avg_pool = nn.AdaptiveAvgPool2d(1)
+        self.max_pool = nn.AdaptiveMaxPool2d(1)
+ 
+        self.fc1   = nn.Conv2d(in_planes, in_planes // 16, 1, bias=False)
+        self.relu1 = nn.ReLU()
+        self.fc2   = nn.Conv2d(in_planes // 16, in_planes, 1, bias=False)
+ 
+        self.sigmoid = nn.Sigmoid()
+ 
+    def forward(self, x):
+        avg_out = self.fc2(self.relu1(self.fc1(self.avg_pool(x))))
+        max_out = self.fc2(self.relu1(self.fc1(self.max_pool(x))))
+        out = avg_out + max_out
+        return self.sigmoid(out)*x
+ 
+#空间注意力模块
+class SpatialAttention(nn.Module):
+    def __init__(self, kernel_size=7):
+        super(SpatialAttention, self).__init__()
+ 
+        assert kernel_size in (3, 7), 'kernel size must be 3 or 7'
+        padding = 3 if kernel_size == 7 else 1
+ 
+        self.conv1 = nn.Conv2d(2, 1, kernel_size, padding=padding, bias=False)
+        self.sigmoid = nn.Sigmoid()
+ 
+    def forward(self, x):
+        ori = x
+        avg_out = torch.mean(x, dim=1, keepdim=True)
+        max_out, _ = torch.max(x, dim=1, keepdim=True)
+        x = torch.cat([avg_out, max_out], dim=1)
+        x = self.conv1(x)
+        return self.sigmoid(x)*ori
+
+
+class CBAMLayer(nn.Module):
+    def __init__(self,channel,reduction = 16,spatial_kernel = 7):
+        super(CBAMLayer,self).__init__()
+        
+        #channel attention 压缩H,W为1
+        self.max_pool = nn.AdaptiveMaxPool2d(1)
+        self.avg_pool = nn.AdaptiveAvgPool2d(1)
+        
+        #shared MLP
+        self.mlp = nn.Sequential(
+            nn.Conv2d(channel,channel//reduction,1,bias = False),
+            nn.ReLU(inplace=True),
+            nn.Conv2d(channel//reduction,channel,1,bias = False)
+        )
+        
+        #spatial attention
+        self.conv = nn.Conv2d(2,1,kernel_size = spatial_kernel,
+                              padding = spatial_kernel//2,bias=False)
+        self.sigmoid = nn.Sigmoid()
+    
+    def forward(self,x):
+        
+        max_out = self.mlp(self.max_pool(x))
+        avg_out = self.mlp(self.avg_pool(x))
+        channel_out = self.sigmoid(max_out+avg_out)
+        x = channel_out * x
+        
+        max_out,_ = torch.max(x,dim = 1,keepdim=True)
+        avg_out = torch.mean(x,dim = 1,keepdim=True)
+        spatial_out = self.sigmoid(self.conv(torch.cat([max_out,avg_out],dim = 1)))
+        x = spatial_out * x
+        return x
+
+class h_sigmoid(nn.Module):
+    def __init__(self, inplace=True):
+        super(h_sigmoid, self).__init__()
+        self.relu = nn.ReLU6(inplace=inplace)
+
+    def forward(self, x):
+        return self.relu(x + 3) / 6
+
+
+class h_swish(nn.Module):
+    def __init__(self, inplace=True):
+        super(h_swish, self).__init__()
+        self.sigmoid = h_sigmoid(inplace=inplace)
+
+    def forward(self, x):
+        return x * self.sigmoid(x)
+
+class CoordAttention(nn.Module):
+
+    def __init__(self, in_channels, out_channels, reduction=16):
+        super(CoordAttention, self).__init__()
+        self.pool_w, self.pool_h = nn.AdaptiveAvgPool2d((1, None)), nn.AdaptiveAvgPool2d((None, 1))
+        temp_c = max(8, in_channels // reduction)
+        self.conv1 = nn.Conv2d(in_channels, temp_c, kernel_size=1, stride=1, padding=0)
+
+        self.bn1 = nn.BatchNorm2d(temp_c)
+        self.act1 = h_swish()
+
+        self.conv2 = nn.Conv2d(temp_c, out_channels, kernel_size=1, stride=1, padding=0)
+        self.conv3 = nn.Conv2d(temp_c, out_channels, kernel_size=1, stride=1, padding=0)
+
+    def forward(self, x):
+        short = x
+        n, c, H, W = x.shape
+        x_h, x_w = self.pool_h(x), self.pool_w(x).permute(0, 1, 3, 2)
+        x_cat = torch.cat([x_h, x_w], dim=2)
+        out = self.act1(self.bn1(self.conv1(x_cat)))
+        x_h, x_w = torch.split(out, [H, W], dim=2)
+        x_w = x_w.permute(0, 1, 3, 2)
+        out_h = torch.sigmoid(self.conv2(x_h))
+        out_w = torch.sigmoid(self.conv3(x_w))
+        return short * out_w * out_h
